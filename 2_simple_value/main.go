21b8c41d395f0f73c229cd47ca44d018dcb87eb8
Sajid-tech <s.khan9430319425@gmail.com>
2026-05-03 19:20:21 +0530

    `simple values`

--- 2_simple_value/main.go
+++ 2_simple_value/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// simple values
+	fmt.Println(1 + 1) //integer value
+	//string
+	fmt.Println("hello golang")
+	//bool
+	fmt.Println(false)
+	//floats
+	fmt.Println(3.14)
+	fmt.Println(7.0 / 3.0)
 }
